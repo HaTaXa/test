0dae50602cd5823fbca4d6090292f2fa96d3e425
HaTaXa <user231082@gmail.com>
2021-04-30 22:51:14 +0300

    Обновление от 30.04.2021
    Выполнено задание с сайта eduprof.ru
    Созданы, обновлены и изменены файлы:
    new file: css/learning3.css
    modified: css/modalWindow.css
    new file: css/modalWindow2.css
    new file: image/close.svg
    deleted: image/img1.png
    modified: index.html
    new file: js/learning3.js
    modified: js/modalWindow.js
    modified: learning1.html
    modified: learning2.html
    new file: learning3.html
    modified: modalWindow.html

---
+++ js/learning3.js
@@ -0,0 +1,126 @@
+//форма с переключателями
+const form = document.getElementById("generateCard");
+//коллекция переключателей
+const radioButtons = document.querySelectorAll("[type='radio']");
+
+let card; //объект - создаваемая карточка
+const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur temporibus maxime doloremque minima? Odio quaerat, et at necessitatibus error animi, impedit libero sit, expedita nulla debitis. Quasi qui ipsa commodi.";
+let imgUrl = ""; //адрес изображения
+
+class Card {
+//конструктор с параметрами, создающий объект карточку (не DOM-объект)
+
+	_id = "";
+	_className = "";
+	_text = "";
+	_imageSource = "";
+
+constructor(id, className, text, imgUrl) {
+	this.Id = id;
+	this.ClassName = className;
+	this.Text = text;
+	this.ImageSource = imgUrl;
+}
+
+get Id() {
+	return this._id;
+}
+
+set Id(value) {
+	this._id = value;
+}
+
+get ClassName() {
+	return this._className;
+}
+
+set ClassName(value) {
+	this._className = value;
+}
+
+get Text() {
+	return this._text;
+}
+
+set Text(value) {
+	this._text = value;
+}
+
+get ImageSource() {
+	return this._imageSource;
+}
+
+set ImageSource(value) {
+	this._imageSource = value;
+}
+
+//метод - создание карточки
+createCard() {
+	let card; //объект - блок (div)
+	let p; //объект - абзац (p)
+
+	card = document.createElement("div");
+	card.id = this.Id;
+	card.className = "card";
+	card.classList.add(this.ClassName);
+
+	form.insertAdjacentElement("afterend", card); //добавляем карточку после формы
+
+	p = document.createElement("p");
+	p.textContent = this.Text; //добавляем текст абзацу
+
+	card.append(p); //добавляем абзац в конец карточки
+
+	//если путь к картинке задан, то создаём её
+	if(typeof this.ImageSource != "undefined") {
+	this.createImage(card);
+	}
+}
+
+//метод - создание изображения в карточке
+createImage(card) {
+	let img; //объект - изображение в карточке
+	
+	img = document.createElement("img");
+	img.src = this.ImageSource;
+	img.alt = "Фото";
+
+	card.prepend(img); //добавляем изображение в начало карточки
+}
+
+//метод - модификация объекта (карточки)
+modifiedCard(typeCard, imgUrl) {  
+	this.ClassName = typeCard;
+	this.ImageSource = imgUrl;
+
+	//удаление изображения в случае выбора "Без фото"
+	if(typeCard == "card-no-photo") {      
+	document.querySelector(".card img").remove();
+	}
+	else {
+	if(!document.querySelector(".card img")) {
+		this.createImage(document.getElementById("card"));
+	}
+	}
+	document.getElementById("card").className = `card ${this.ClassName}`;
+}
+}
+
+for(let radio of radioButtons) {
+radio.addEventListener("change", () => {
+	if(radio.value == "card-no-photo") {
+		imgUrl = undefined;
+	}
+	else {
+	imgUrl = "https://picsum.photos/500/300";
+	}
+
+	if(document.getElementById("card")) {
+	card.modifiedCard(radio.value, imgUrl);
+	}
+	else {
+	card = new Card("card", radio.value, text, imgUrl);
+	card.createCard();
+	}
+});
+}
--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,138 +1,34 @@
-// const form = {
-// 	type: "form",
-// 	title: "Modal Window",
-// 	text: "Modal Window Text",
-// 	buttonOK: "«Enter»",
-// 	buttonCancel: "Esc",
-// 	style: {
-// 		position: "absolute",
-// 		width: "300px",
-// 		height: "200px",
-// 		display: "flex",
-// 		margin: "auto",
-// 		padding: "10px",
-// 		border: "2px solid gold",
-// 		borderRadius: "10px",
-// 		outlineColor: "black",
-// 		alignItems: "center",
-// 		fontFamily: "Verdana, sans-serif",
-// 		backgroundColor: "darkred"
-// 	},
-// 	getType() {
-// 		return this.type;
-// 	},
-// 	getTitle() {
-// 		return this.title;
-// 	},
-// 	getText() {
-// 		return this.text;
-// 	},
-// 	getStyle() {
-// 		return this.style
-// 	}
-// }
+// Подробно изучено тут:
+// https://smartlanding.biz/kak-sdelat-modalnoe-okno-na-sajte.html
+// (i) Потому что я не понимаю нагромождение кода дополнительным синтаксисом.
 
-let inputButton = document.getElementById('btn-demoButton');
+$(function () {
+// Закрытие модального окна при клике вне его контентной области
+	// $('.modal').mouseup(function(e) {
+	// 	let modalContent = $(".modal__content");
+	// 	if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+	// 	$(this).removeClass('modal_active');
+	// 	}
+	// });
 
-let formModalWindow = document.getElementById('form-modalWindow');
-/* let btnOK = document.getElementById('btn-ok'); */
-let btnCancel = document.getElementById('btn-cancel');
-
-
-
-// ---
-inputButton.onmouseover = function(event) {
-	inputButton.style.cursor = 'pointer';
-}
-
-inputButton.onmouseout = function(event) {
-	inputButton.style.cursor = 'auto';
-}
-// ---
-
-// btnOK.onmouseover = function(event) {
-// 	btnOK.style.cursor = 'pointer';
-// }
-
-// btnOK.onmouseout = function(event) {
-// 	btnOK.style.cursor = 'auto';
-// }
-
-// ---
-document.addEventListener("keydown", function(event) {
-	if (event.key == "Escape") {
-		// не работает как надо - не понимаю
-		if($(".form").hasClass("form-modal-hidden")) {
-			inputButton.setAttribute("disabled", "disabled"); // кнопка недоступна
+// Нажатие клавиши Esc.
+// Если класс существует, значит модальное окно выведено на дисплей, => надо удалить класс, чтобы скрыть модальное окно по нажатию на esc
+	$(document).keyup(function(e) {
+		if (e.key === "Escape" || e.keyCode === 27) {
+			if ($('.modal').hasClass("modal_active")) {
+				$('.modal').removeClass('modal_active');
+				console.log(`removeClass('modal_active')`);
+			}
 		}
-		else if(!$(".form").hasClass("form-modal-hidden")) {
-			formShowHide("form-modal-hidden");
-			console.log(`Escape`);
-		}
-
-		// formShowHide("form-modal-hidden");
-		// inputButton.removeAttribute("disabled");
-		// inputButton.setAttribute("disabled", "enabled"); // не срабатывает
-		// inputButton.setAttribute("disabled", "true"); // не срабатывает
-
-		// не срабатывает - не понимаю как правильно прописывать
-		// $(inputButton).removeAttribute("disabled"); // делаем активной
-	}
+	});
+// Добавляем класс модальному окну, чтобы оно появилось
+	$('#callback-button').click(function() {
+		$('.modal').addClass('modal_active');
+		console.log(`addClass('modal_active')`);
+	});
+// Удаляем класс модальному окну, чтобы оно исчезло
+	$('.modal__close-button').click(function() {
+		$('.modal').removeClass('modal_active');
+		console.log(`removeClass('modal_active')`);
+	});
 });
-
-inputButton.addEventListener ("click", () => {
-	formShowHide("form-modal-hidden");
-	// inputButton.setAttribute("disabled", "disabled");
-	// formModalWindow.classList.remove("form-modal-hidden"); // можно не делать remove
-	
-	// не срабатывает - не понимаю как правильно прописывать
-	// $(formModalWindow).removeClass('form-modal-hidden'); // можно не делать remove
-	// $(inputButton).setAttribute("disabled", "disabled"); // кнопка недоступна
-});
-
-btnCancel.onmouseover = function(event) {
-	btnCancel.style.cursor = 'pointer';
-}
-
-btnCancel.onmouseout = function(event) {
-	btnCancel.style.cursor = 'auto';
-}
-
-btnCancel.addEventListener("click", () => {
-	formShowHide("form-modal-hidden");
-	inputButton.removeAttribute("disabled");
-	// formModalWindow.classList.add("form-modal-hidden");
-	// inputButton.setAttribute("disabled", "enabled"); // не срабатывает
-	// inputButton.setAttribute("disabled", "false"); // не срабатывает
-
-	// не срабатывает - не понимаю как правильно прописывать
-	// $(inputButton).removeAttribute("disabled"); // делаем активной
-});
-
-function formShowHide(attribute) {
-	if (console.log(formModalWindow.classList.toggle(attribute))) {
-		inputButton.setAttribute("disabled", "disabled"); // кнопка недоступна
-	}
-	else {
-		inputButton.removeAttribute("disabled"); // делаем активной
-	}
-	
-	// if(!$(".form").hasClass("form-modal-hidden")) {
-	// 	inputButton.removeAttribute("disabled"); // делаем активной
-	// }
-	// else if($(".form").hasClass("form-modal-hidden")) {
-	// 	inputButton.setAttribute("disabled", "disabled"); // кнопка недоступна
-	// }
-
-	// console.log($(inputButton).getAttribute("disabled", "disabled"));
-
-	// $(formModalWindow).classList.toggle(attribute); // не срабатывает
-
-	// не срабатывает - не понимаю как правильно прописывать
-	// if ($("inputButton").attr('disabled','disabled')) {
-	// 	inputButton.removeAttribute("disabled");
-	// }
-	// else {
-	// 	$(inputButton).setAttribute("disabled", "disabled");
-	// }
-}
