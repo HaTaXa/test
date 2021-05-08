d9301af4b80472a655e4fbc86840f2a2b59c9d11
HaTaXa <user231082@gmail.com>
2021-05-08 09:35:28 +0300

    Обновление от 08.05.2021
    Изменено расположение папапок и файлов.
    Создано и добавлено новое задание «manualPage.html».

--- examples/js/workingWithEvents.js
+++ examples/js/workingWithEvents.js
@@ -1,6 +1,6 @@
 /***** DOM-объекты (начало) *****/
 let body = document.body; // тело страницы
-let titles = document.querySelectorAll(".title") // заголовок и названия областей формы
+let titles = document.querySelectorAll(".title"); // заголовок и названия областей формы
 
 /* ПОЛЯ ВВОДА (числа, переключатели, флажки) */
 let inputs = document.querySelectorAll(".parametrs input"); // коллекция полей (поля ввода и кнопки)
@@ -35,11 +35,11 @@ let player; // создаваемый в DOM объект плеер
 
 /* объект, содержащий свойства создаваемого плеера */
 let playerAttributes = {
-    "id": "player",
-    "src": "media/sample.mp3",
-    "controls": "",
-    "autoplay": "",
-    "class": "player"
+	id: "player",
+	src: "media/sample.mp3",
+	controls: "",
+	autoplay: "",
+	class: "player",
 };
 
 let result; // результат вычисления
@@ -50,267 +50,267 @@ let solution; // создаваемый в DOM объект (абзац) для 
 
 // обработчик события "change" при изменении цвета фона
 inputPageBgColor.addEventListener("input", () => {
-body.style.backgroundColor = inputPageBgColor.value;
+	body.style.backgroundColor = inputPageBgColor.value;
 
-for (let title of titles) {
-    title.style.color = body.style.backgroundColor;
-    title.style.filter = "invert(75%) hue-rotate(270deg)";
-}
-})
+	for (let title of titles) {
+		title.style.color = body.style.backgroundColor;
+		title.style.filter = "invert(75%) hue-rotate(270deg)";
+	}
+});
 
 // обработчик события "input" при загрузки фонового изображения
 inputPageImgLink.addEventListener("change", () => {
-indexChecked = getIndexChecked(bgSize);
-setBgPage(indexChecked);
-})
+	indexChecked = getIndexChecked(bgSize);
+	setBgPage(indexChecked);
+});
 
 // обработчик события "input" при выборе обычного масштаба
 bgSize[0].addEventListener("input", () => {
-setBgPage(0);
-})
+	setBgPage(0);
+});
 
 // обработчик события "input" при выборе масштабирования фона
 bgSize[1].addEventListener("input", () => {
-setBgPage(1);
-})
+	setBgPage(1);
+});
 
 // обработчик события "input" при выборе мозаичного фона
 bgSize[2].addEventListener("input", () => {
-setBgPage(2);
-})
+	setBgPage(2);
+});
 
-// обработчик события "input" при вводе в поле коэф. a 
+// обработчик события "input" при вводе в поле коэф. a
 inputParamA.addEventListener("input", () => {
-paramA = updateParam(inputParamA, rangeParamA);
-unBlocked(inputParamB, rangeParamB, buttons);
-})
+	paramA = updateParam(inputParamA, rangeParamA);
+	unBlocked(inputParamB, rangeParamB, buttons);
+});
 
-// обработчик события "change" при изменении ползунка коэф. a 
+// обработчик события "change" при изменении ползунка коэф. a
 rangeParamA.addEventListener("change", () => {
-paramA = updateParam(rangeParamA, inputParamA);
-unBlocked(inputParamB, rangeParamB, buttons);
-})
+	paramA = updateParam(rangeParamA, inputParamA);
+	unBlocked(inputParamB, rangeParamB, buttons);
+});
 
 // обработчик события "input" при вводе в поле коэф. b
 inputParamB.addEventListener("input", () => {
-paramB = updateParam(inputParamB, rangeParamB);
-unBlocked(inputParamC, rangeParamC);
-})
+	paramB = updateParam(inputParamB, rangeParamB);
+	unBlocked(inputParamC, rangeParamC);
+});
 
-// обработчик события "change" при изменении ползунка коэф. b 
+// обработчик события "change" при изменении ползунка коэф. b
 rangeParamB.addEventListener("change", () => {
-paramB = updateParam(rangeParamB, inputParamB);
-unBlocked(inputParamC, rangeParamC);
-})
+	paramB = updateParam(rangeParamB, inputParamB);
+	unBlocked(inputParamC, rangeParamC);
+});
 
 // обработчик события "input" при вводе в поле коэф. c
 inputParamC.addEventListener("input", () => {
-paramC = updateParam(inputParamC, rangeParamC);
-})
+	paramC = updateParam(inputParamC, rangeParamC);
+});
 
-// обработчик события "change" при изменении ползунка коэф. c 
+// обработчик события "change" при изменении ползунка коэф. c
 rangeParamC.addEventListener("change", () => {
-paramC = updateParam(rangeParamC, inputParamC);
-})
+	paramC = updateParam(rangeParamC, inputParamC);
+});
 
 // обработчик события "click" при клике по кнопке "Произвести расчёт"
 btnCalc.addEventListener("click", () => {
-result = calcSolution(paramA, paramB, paramC);
-printSolution();
-})
+	result = calcSolution(paramA, paramB, paramC);
+	printSolution();
+});
 
 // обработчик события "click" при клике по кнопке "Очистить"
 btnReset.addEventListener("click", () => {
-for(let item of inputs) {
-    if (item.getAttribute("type") == "number" || item.getAttribute("type") == "range") {
-        item.value = "";
-    }
-    if(item.getAttribute("id") == "param_a" || item.getAttribute("id") == "range_a" || item.getAttribute("id") == "btn_play") {
-        continue;
-    }
-    else {
-        item.setAttribute("disabled", "disabled");
-    }
-}
-body.removeChild(solution);
-})
+	for (let item of inputs) {
+		if (
+			item.getAttribute("type") == "number" ||
+			item.getAttribute("type") == "range"
+		) {
+			item.value = "";
+		}
+		if (
+			item.getAttribute("id") == "param_a" ||
+			item.getAttribute("id") == "range_a" ||
+			item.getAttribute("id") == "btn_play"
+		) {
+			continue;
+		} else {
+			item.setAttribute("disabled", "disabled");
+		}
+	}
+	body.removeChild(solution);
+});
 
 // обработчик события "click" при клике по кнопке "Показать/удалить плеер"
 btnPlay.addEventListener("click", () => {
-if(document.getElementById("player")) {
-    body.removeChild(player);
-    btnPlay.setAttribute("value", "Показать плеер");
-}
-else {
-    player = createPlayer("audio", playerAttributes);
+	if (document.getElementById("player")) {
+		body.removeChild(player);
+		btnPlay.setAttribute("value", "Показать плеер");
+	} else {
+		player = createPlayer("audio", playerAttributes);
 
-    body.append(player);
-    btnPlay.setAttribute("value", "Удалить плеер");
-}
-})
+		body.append(player);
+		btnPlay.setAttribute("value", "Удалить плеер");
+	}
+});
 /***** ОБРАБОТЧИКИ СОБЫТИЙ (конец) *****/
 
 /***** ФУНКЦИИ *****/
 // функция, возвращающая порядковый номер выбранного переключателя
-function getIndexChecked(bgSize) { 
-for (let i = 0; i < bgSize.length; i++) {
-    if (bgSize[i].checked) {
-        return i;
-    }
-}
+function getIndexChecked(bgSize) {
+	for (let i = 0; i < bgSize.length; i++) {
+		if (bgSize[i].checked) {
+			return i;
+		}
+	}
 }
 
 // функция, задающая изображение фона страницы
 function setBgPage(checked) {
-body.style.backgroundImage = `url("${inputPageImgLink.value}")`;
+	body.style.backgroundImage = `url("${inputPageImgLink.value}")`;
 
-switch(checked) {
-    case 0: body.style.backgroundRepeat = "no-repeat";
-            body.style.backgroundSize = "1920px";
-            break;
-    case 1: body.style.backgroundRepeat = "no-repeat";
-            body.style.backgroundSize = "cover";
-            break;
-    case 2: body.style.backgroundSize = "auto";
-            body.style.backgroundRepeat = "repeat";
-            break;
-}
+	switch (checked) {
+		case 0:
+			body.style.backgroundRepeat = "no-repeat";
+			body.style.backgroundSize = "1920px";
+			break;
+		case 1:
+			body.style.backgroundRepeat = "no-repeat";
+			body.style.backgroundSize = "cover";
+			break;
+		case 2:
+			body.style.backgroundSize = "auto";
+			body.style.backgroundRepeat = "repeat";
+			break;
+	}
 }
 
 // функция разблокировки полей и кнопок
 function unBlocked(input, range, buttons) {
-input.removeAttribute("disabled");
-range.removeAttribute("disabled");
+	input.removeAttribute("disabled");
+	range.removeAttribute("disabled");
 
-if(buttons) {
-    for (let btn of buttons) {
-        btn.removeAttribute("disabled");
-    }
-}
+	if (buttons) {
+		for (let btn of buttons) {
+			btn.removeAttribute("disabled");
+		}
+	}
 }
 
 // функция установки/обновления коэфициента в поле/ползунке
 function updateParam(input1, input2) {
-let param = +input1.value; //значение коэф.
-input2.value = param; //значение поля/ползунка с коэф.
+	let param = +input1.value; //значение коэф.
+	input2.value = param; //значение поля/ползунка с коэф.
 
-return param;
+	return param;
 }
 
 // функция создания аудио-плеера
 function createPlayer(tag, attr) {
-let player = document.createElement(tag);
+	let player = document.createElement(tag);
 
-for (let key in attr) {
-    player.setAttribute(key, attr[key]);
-}
-player.classList.add("player-show");
+	for (let key in attr) {
+		player.setAttribute(key, attr[key]);
+	}
+	player.classList.add("player-show");
 
-return player;
+	return player;
 }
 
 // главная функция расчёта корней (вычисление)
 function calcSolution(a, b, c) {
-let D; //дискриминант
-let result; //строка с итогом
+	let D; //дискриминант
+	let result; //строка с итогом
 
-if (typeof b == "undefined") {
-    b = 0;
-}
-if (typeof c == "undefined") {
-    c = 0;
-}
+	if (typeof b == "undefined") {
+		b = 0;
+	}
+	if (typeof c == "undefined") {
+		c = 0;
+	}
 
-if(a == 0) {
-    if(b == 0) {
-        result = "Корней нет!";
-    }
-    else {
-        if(c != 0) {
-            result = -c / b;
-        }
-        else {
-            result = 0;
-        }
-    }
-}
-else if(b == 0) {
-    if(c != 0) {
-        (-c / a >= 0) ? result = Math.sqrt(-c / a) : result = "Корней нет!";
-    }
-    else {
-        result = 0;
-    }
-}
-else if(c == 0) {
-    result = [0, -b / a];
-}
-else {
-    D = calcD(a, b, c);
-    result = calcRoots(D, a, b, c);
-}
-return result;
+	if (a == 0) {
+		if (b == 0) {
+			result = "Корней нет!";
+		} else {
+			if (c != 0) {
+				result = -c / b;
+			} else {
+				result = 0;
+			}
+		}
+	} else if (b == 0) {
+		if (c != 0) {
+			-c / a >= 0 ? (result = Math.sqrt(-c / a)) : (result = "Корней нет!");
+		} else {
+			result = 0;
+		}
+	} else if (c == 0) {
+		result = [0, -b / a];
+	} else {
+		D = calcD(a, b, c);
+		result = calcRoots(D, a, b, c);
+	}
+	return result;
 }
 
 // функция для расчёта дискриминанта
 function calcD(a, b, c) {
-return b ** 2 - 4 * a * c;
+	return b ** 2 - 4 * a * c;
 }
 
 // функция для расчёта корней квадратного уравнения
 function calcRoots(D, a, b, c) {
-let x1, x2; //корни квадратного уравнения
+	let x1, x2; //корни квадратного уравнения
 
-if(D > 0) {
-    x1 = (-b + Math.sqrt(D)) / (2 * a);
-    x2 = (-b - Math.sqrt(D)) / (2 * a);
+	if (D > 0) {
+		x1 = (-b + Math.sqrt(D)) / (2 * a);
+		x2 = (-b - Math.sqrt(D)) / (2 * a);
 
-    return [x1, x2];
-}
-else if (D == 0) {
-    return -b / (2 * a);
-}
-else {
-    return "Корней нет!";
-}
+		return [x1, x2];
+	} else if (D == 0) {
+		return -b / (2 * a);
+	} else {
+		return "Корней нет!";
+	}
 }
 
 // функция вывода результа на страницу (в создаваемый объект solution - абзац)
 function printSolution() {
-if (document.querySelector(".solution")) {
-    solution.innerHTML = checkResult(result);
-}
-else {
-    solution = createElem("p", checkResult(result));
-    solution.innerHTML = checkResult(result);
-    body.append(solution);
-}
+	if (document.querySelector(".solution")) {
+		solution.innerHTML = checkResult(result);
+	} else {
+		solution = createElem("p", checkResult(result));
+		solution.innerHTML = checkResult(result);
+		body.append(solution);
+	}
 }
 
 // функция создания элемента
 function createElem(tag, content) {
-let elem;
+	let elem;
 
-elem = document.createElement(tag);
-elem.innerHTML = content;
-elem.classList.add("solution");
+	elem = document.createElement(tag);
+	elem.innerHTML = content;
+	elem.classList.add("solution");
 
-return elem;
+	return elem;
 }
 
 // функция проверки результата вычисления
 function checkResult(result) {
-if (typeof result == "string") {
-    return `<strong>${result}</strong>`;
+	if (typeof result == "string") {
+		return `<strong>${result}</strong>`;
+	} else if (typeof result == "number") {
+		return `Уравнение имеет один корень: <strong>x = ${result.toFixed(
+			2
+		)}</strong>`;
+	} else {
+		return `Уравнение квадратное.<br>Имеет два корня: <strong>x1 = ${result[0].toFixed(
+			2
+		)}; x2 = ${result[1].toFixed(2)}</strong>`;
+	}
 }
-else if (typeof result == "number") {
-    return `Уравнение имеет один корень: <strong>x = ${result.toFixed(2)}</strong>`;
-}
-else {
-    return `Уравнение квадратное.<br>Имеет два корня: <strong>x1 = ${result[0].toFixed(2)}; x2 = ${result[1].toFixed(2)}</strong>`;
-}
-}
-
 
 /***** КОД при использовании alert и prompt *****/
 // let params = setParametrs();
---
+++ h&m/js/manualPage.js
@@ -0,0 +1,74 @@
+
+
+// кнопка Быстрый поиск
+function remoteSearch() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Обзор
+function topicsPane() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Слова
+function indexPane() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Поиск
+function searchPane() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Открепить
+function undockTab() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Новая вкладка
+function newTab() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Показать/Скрыть
+function bannerShowHide() {
+	// alert(`(i) Пока что в разработке`);
+	let elemBanner = document.getElementById("idBanner");
+	let elemToolbar = document.getElementById('idToolbar');
+
+	/* if ((elemBanner.style.display == 'none') && $(elemToolbar).hasClass('compact-toolbar')) { */
+	if ($(elemBanner).css('display') == 'none') {
+		// console.log(`стиль`);
+		// elemBanner.style.display = 'block';
+		$(elemBanner).css('display', 'block');
+	}
+	else if ($(elemBanner).css('display') == 'block') {
+		$(elemBanner).css('display', 'none');
+	}
+
+	if ($(elemToolbar).hasClass('compact-toolbar')) {
+		// console.log(`класс`);
+		$(elemToolbar).removeClass('compact-toolbar').addClass('banner-toolbar');
+	}
+	else if ($(elemToolbar).hasClass('banner-toolbar')) {
+		$(elemToolbar).removeClass('banner-toolbar').addClass('compact-toolbar');
+	}
+}
+// кнопка Развернуть/Свернуть
+function expandHiddenText() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Постоянная ссылка
+function setPermalink() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Печать
+function printTopic() {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Домой
+function pageHome(hrefString) {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Назад
+function pagePrevious(hrefString) {
+	alert(`(i) Пока что в разработке`);
+}
+// кнопка Вперед
+function pageNext(hrefString) {
+	alert(`(i) Пока что в разработке`);
+}
